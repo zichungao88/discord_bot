f025fe2dfcf663eeeafc040c09c2cf08406beae0
Zichun (Jerry) Gao <zgaobear@gmail.com>
2022-06-28 22:55:17 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({intents:["GUILDS", "GUILD_MESSAGES"]});
+
+client.once('ready', () => {
+    console.log('Your JG Vlogs Personal Assistant is online!');
+});
+
+client.login('OTkxNTE4MDEwMDM4ODI1MDYy.GMV-6c.EkfroZeOBX5dZIwqJ9qEUTT5NVLStuh2pCGs5g');
